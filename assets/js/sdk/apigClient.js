c6441cdb631e42ca0da519d49597c53c74699e6c
harjot1996 <harjot1996@users.noreply.github.com>
2021-10-08 21:38:53 -0400

    Update apigClient.js

--- assets/js/sdk/apigClient.js
+++ assets/js/sdk/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
 
     // extract endpoint and path from url
-    var invokeUrl = 'http://wineandfinedine.s3-website.us-east-2.amazonaws.com/v1';
+    var invokeUrl = 'https://fni62e28te.execute-api.us-east-2.amazonaws.com/v1';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
